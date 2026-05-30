ab2151a6dd05e148d1d9826c7d45967b2ba4629d
jorge75 <jorgecanelom75@gmail.com>
2026-05-30 17:59:25 -0400

    delete is done

--- newspapers/views.py
+++ newspapers/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import blog
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
 
 class PostListView(ListView):
     model = blog
@@ -19,3 +21,8 @@ class PostUpdateView(UpdateView):
     model = blog
     template_name = 'post-update.html'
     fields = ['title', 'content']
+
+class PostDeleteView(DeleteView):
+    model = blog
+    template_name = 'post-delete.html'
+    success_url = reverse_lazy('post_list')
